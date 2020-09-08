a64c1adcbfd6e1cecb7d8a3b2ee7b2fda84b8abe
Amifa Raj <amifaraj@amifas-mbp.boisestate.edu>
2020-09-08 01:38:32 -0600

    Project page updated

--- _site/assets/js/lunr/lunr-store.js
+++
@@ -1,6 +0,0 @@
-var store = [{
-        "title": "Comparing Fair Ranking Metrics",
-        "excerpt":"Amifa Raj, Connor Wood, Ananda Montoly and Michael D. Ekstrand. 2020. “Comparing Fair Ranking Metrics”. To appear in Proceedings of the 3rd FAccTRec Workshop on Responsible Recommendation at 14th ACM Conference on Recommender Systems (RecSys 20). Abstract Ranking is a fundamental aspect of recommender systems. However, ranked outputs can be...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/pubs/2020/FAccTRec20-CompareMetrics",
-        "teaser":null}]
